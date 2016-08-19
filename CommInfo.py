04354a8ab9ae6c54aa32b3f32cca369d52cc244c
Zhang Hongtao <htao_zhang@126.com>
2016-08-19 14:17:57 +0800

    Update CommInfo.py

--- CommInfo.py
+++ CommInfo.py
@@ -4,36 +4,5 @@
 
 ALLNULL = ['NULL', 'Null', 'null', None, '', 'None', ' ']
 
-class onlinedb:
-    host = '10.10.87.87'
-    port = 3306
-    db = 'onlinedb'
-    user = 'root'
-    passwd = 'miaoji@2014!'
-    charset = 'utf-8'
-
-class devdb:
-    host = '10.10.87.87'
-    port = 3306
-    db = 'devdb'
-    user = 'root'
-    passwd = 'miaoji@2014!'
-    charset = 'utf-8'
-
-class crawl:
-    host = '10.10.92.245'
-    port = 3306
-    db = 'crawl'
-    user = 'root'
-    passwd = 'miaoji@2014!'
-    charset = 'utf-8'
- 
-class workload:
-    host = '10.10.86.250'
-    port = 3306
-    db = 'workload'
-    user = 'root'
-    passwd = 'miaoji@2014!'
-    charset = 'utf-8'
 
 
